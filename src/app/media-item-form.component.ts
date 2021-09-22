702cc016243496c6521796eb2fef3e9a33c1c531
Vignesh KRV <vignesh1105krv@gmail.com>
2021-09-22 14:58:27 -0500

    custom validator

--- src/app/media-item-form.component.ts
+++ src/app/media-item-form.component.ts
@@ -1,24 +1,41 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'mw-media-item-form',
-  templateUrl: './media-item-form.component.html',
-  styleUrls: ['./media-item-form.component.css']
+  selector: "mw-media-item-form",
+  templateUrl: "./media-item-form.component.html",
+  styleUrls: ["./media-item-form.component.css"],
 })
 export class MediaItemFormComponent implements OnInit {
   form: FormGroup;
 
   ngOnInit() {
     this.form = new FormGroup({
-      medium: new FormControl('Movies'),
-      name: new FormControl('', Validators.compose([
-        Validators.required,
-        Validators.pattern('[\\w\\-\\s\\/]+')
-      ])),
-      category: new FormControl(''),
-      year: new FormControl(''),
+      medium: new FormControl("Movies"),
+      name: new FormControl(
+        "",
+        Validators.compose([
+          Validators.required,
+          Validators.pattern("[\\w\\-\\s\\/]+"),
+        ])
+      ),
+      category: new FormControl(""),
+      year: new FormControl("", this.yearValidator),
     });
+  }
+  yearValidator(control: FormControl) {
+    //custom validator return null when valid and object when invalid
+    if (control.value.trim().length === 0) {
+      return null;
+    }
+    const year = parseInt(control.value, 10);
+    const minYear = 1900;
+    const maxYear = 2100;
+    if (year >= minYear && year <= maxYear) {
+      return null;
+    } else {
+      return { year: true };
+    }
   }
 
   onSubmit(mediaItem) {
