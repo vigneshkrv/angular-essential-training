cb1580c8edb0a272b8225d7149a79b327e64615c
Vignesh KRV <vignesh1105krv@gmail.com>
2021-09-23 15:43:42 -0500

    use Injectable decorator for root inject

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,24 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { ReactiveFormsModule } from '@angular/forms';
-import { AppComponent } from './app.component';
-import { MediaItemComponent } from './media-item.component';
-import { MediaItemListComponent } from './media-item-list.component';
-import { FavoriteDirective } from './favorite.directive';
-import { CategoryListPipe } from './category-list.pipe';
-import { MediaItemFormComponent } from './media-item-form.component';
-import { MediaItemService } from './media-item.service';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { ReactiveFormsModule } from "@angular/forms";
+import { AppComponent } from "./app.component";
+import { MediaItemComponent } from "./media-item.component";
+import { MediaItemListComponent } from "./media-item-list.component";
+import { FavoriteDirective } from "./favorite.directive";
+import { CategoryListPipe } from "./category-list.pipe";
+import { MediaItemFormComponent } from "./media-item-form.component";
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    ReactiveFormsModule
-  ],
+  imports: [BrowserModule, ReactiveFormsModule],
   declarations: [
     AppComponent,
     MediaItemComponent,
     MediaItemListComponent,
     FavoriteDirective,
     CategoryListPipe,
-    MediaItemFormComponent
+    MediaItemFormComponent,
   ],
-  providers: [
-    MediaItemService
-  ],
-  bootstrap: [
-    AppComponent
-  ]
+
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/media-item.service.ts
+++ src/app/media-item.service.ts
@@ -1,47 +1,54 @@
+import { Injectable } from "@angular/core";
+@Injectable({
+  providedIn: "root",
+})
 export class MediaItemService {
   mediaItems = [
     {
       id: 1,
-      name: 'Firebug',
-      medium: 'Series',
-      category: 'Science Fiction',
+      name: "Firebug",
+      medium: "Series",
+      category: "Science Fiction",
       year: 2010,
       watchedOn: 1294166565384,
-      isFavorite: false
+      isFavorite: false,
     },
     {
       id: 2,
-      name: 'The Small Tall',
-      medium: 'Movies',
-      category: 'Comedy',
+      name: "The Small Tall",
+      medium: "Movies",
+      category: "Comedy",
       year: 2015,
       watchedOn: null,
-      isFavorite: true
-    }, {
+      isFavorite: true,
+    },
+    {
       id: 3,
-      name: 'The Redemption',
-      medium: 'Movies',
-      category: 'Action',
+      name: "The Redemption",
+      medium: "Movies",
+      category: "Action",
       year: 2016,
       watchedOn: null,
-      isFavorite: false
-    }, {
+      isFavorite: false,
+    },
+    {
       id: 4,
-      name: 'Hoopers',
-      medium: 'Series',
-      category: 'Drama',
+      name: "Hoopers",
+      medium: "Series",
+      category: "Drama",
       year: null,
       watchedOn: null,
-      isFavorite: true
-    }, {
+      isFavorite: true,
+    },
+    {
       id: 5,
-      name: 'Happy Joe: Cheery Road',
-      medium: 'Movies',
-      category: 'Action',
+      name: "Happy Joe: Cheery Road",
+      medium: "Movies",
+      category: "Action",
       year: 2015,
       watchedOn: 1457166565384,
-      isFavorite: false
-    }
+      isFavorite: false,
+    },
   ];
 
   get() {
