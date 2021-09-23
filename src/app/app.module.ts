36187a1ab828e0b3337a7fe9e0b0733e152657bf
Vignesh KRV <vignesh1105krv@gmail.com>
2021-09-23 14:52:58 -0500

    create service mediaItemService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,25 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { ReactiveFormsModule } from '@angular/forms';
-import { AppComponent } from './app.component';
-import { MediaItemComponent } from './media-item.component';
-import { MediaItemListComponent } from './media-item-list.component';
-import { FavoriteDirective } from './favorite.directive';
-import { CategoryListPipe } from './category-list.pipe';
-import { MediaItemFormComponent } from './media-item-form.component';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { ReactiveFormsModule } from "@angular/forms";
+import { AppComponent } from "./app.component";
+import { MediaItemComponent } from "./media-item.component";
+import { MediaItemListComponent } from "./media-item-list.component";
+import { FavoriteDirective } from "./favorite.directive";
+import { CategoryListPipe } from "./category-list.pipe";
+import { MediaItemFormComponent } from "./media-item-form.component";
+import { MediaItemService } from "./media-item.service";
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    ReactiveFormsModule
-  ],
+  imports: [BrowserModule, ReactiveFormsModule],
   declarations: [
     AppComponent,
     MediaItemComponent,
     MediaItemListComponent,
     FavoriteDirective,
     CategoryListPipe,
-    MediaItemFormComponent
+    MediaItemFormComponent,
   ],
-  bootstrap: [
-    AppComponent
-  ]
+  bootstrap: [AppComponent],
+  providers: [MediaItemService],
 })
 export class AppModule {}
