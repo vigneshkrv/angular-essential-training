f700ccc1b8dbaa8e2276aa933a6a45b5f0be0407
Vignesh KRV <vignesh1105krv@gmail.com>
2021-09-23 17:52:31 -0500

    import HttpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,26 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { ReactiveFormsModule } from '@angular/forms';
-import { AppComponent } from './app.component';
-import { MediaItemComponent } from './media-item.component';
-import { MediaItemListComponent } from './media-item-list.component';
-import { FavoriteDirective } from './favorite.directive';
-import { CategoryListPipe } from './category-list.pipe';
-import { MediaItemFormComponent } from './media-item-form.component';
-import { lookupListToken, lookupLists } from './providers';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { ReactiveFormsModule } from "@angular/forms";
+import { AppComponent } from "./app.component";
+import { MediaItemComponent } from "./media-item.component";
+import { MediaItemListComponent } from "./media-item-list.component";
+import { FavoriteDirective } from "./favorite.directive";
+import { CategoryListPipe } from "./category-list.pipe";
+import { MediaItemFormComponent } from "./media-item-form.component";
+import { lookupListToken, lookupLists } from "./providers";
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    ReactiveFormsModule
-  ],
+  imports: [BrowserModule, ReactiveFormsModule, HttpClientModule],
   declarations: [
     AppComponent,
     MediaItemComponent,
     MediaItemListComponent,
     FavoriteDirective,
     CategoryListPipe,
-    MediaItemFormComponent
+    MediaItemFormComponent,
   ],
-  providers: [
-    { provide: lookupListToken, useValue: lookupLists }
-  ],
-  bootstrap: [
-    AppComponent
-  ]
+  providers: [{ provide: lookupListToken, useValue: lookupLists }],
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
