b4ef0d69cafc695fbdc77093774c7a2e4129df89
Vignesh KRV <vignesh1105krv@gmail.com>
2021-09-27 16:03:01 -0500

    feature NgModule for routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,38 +1,31 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule, HttpXhrBackend } from '@angular/common/http';
-import { AppComponent } from './app.component';
-import { MediaItemComponent } from './media-item.component';
-import { MediaItemListComponent } from './media-item-list.component';
-import { FavoriteDirective } from './favorite.directive';
-import { CategoryListPipe } from './category-list.pipe';
-import { MediaItemFormComponent } from './media-item-form.component';
-import { lookupListToken, lookupLists } from './providers';
-import { MockXHRBackend } from './mock-xhr-backend';
-import { routing } from './app.routing';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+
+import { HttpClientModule, HttpXhrBackend } from "@angular/common/http";
+import { AppComponent } from "./app.component";
+import { MediaItemComponent } from "./media-item.component";
+import { MediaItemListComponent } from "./media-item-list.component";
+import { FavoriteDirective } from "./favorite.directive";
+import { CategoryListPipe } from "./category-list.pipe";
+
+import { lookupListToken, lookupLists } from "./providers";
+import { MockXHRBackend } from "./mock-xhr-backend";
+import { routing } from "./app.routing";
+import { NewItemModule } from "./new-item/new-item.module";
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    ReactiveFormsModule,
-    HttpClientModule,
-    routing
-  ],
+  imports: [BrowserModule, HttpClientModule, routing, NewItemModule],
   declarations: [
     AppComponent,
     MediaItemComponent,
     MediaItemListComponent,
     FavoriteDirective,
     CategoryListPipe,
-    MediaItemFormComponent
   ],
   providers: [
     { provide: lookupListToken, useValue: lookupLists },
-    { provide: HttpXhrBackend, useClass: MockXHRBackend }
+    { provide: HttpXhrBackend, useClass: MockXHRBackend },
   ],
-  bootstrap: [
-    AppComponent
-  ]
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
