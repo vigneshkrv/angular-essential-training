effaae5130712e822700fb9cc0d2df0879a4a32a
Vignesh KRV <vignesh1105krv@gmail.com>
2021-09-27 15:20:53 -0500

    configure routes

---
+++ src/app/app.routing.ts
@@ -0,0 +1,10 @@
+import { Routes } from "@angular/router";
+import { MediaItemFormComponent } from "./media-item-form.component";
+import { MediaItemListComponent } from "./media-item-list.component";
+import { MediaItemComponent } from "./media-item.component";
+
+const appRoutes: Routes = [
+  { path: "add", component: MediaItemFormComponent },
+  { path: ":medium", component: MediaItemListComponent },
+  { path: "", redirectTo: "all", pathMatch: "full" },
+];
