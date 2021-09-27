aefb7cd254e5d0daadddfc0a915b58abbcdf2829
Vignesh KRV <vignesh1105krv@gmail.com>
2021-09-27 16:52:32 -0500

    Lazy laoding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,28 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule, HttpXhrBackend } from '@angular/common/http';
-import { AppComponent } from './app.component';
-import { MediaItemComponent } from './media-item.component';
-import { MediaItemListComponent } from './media-item-list.component';
-import { FavoriteDirective } from './favorite.directive';
-import { CategoryListPipe } from './category-list.pipe';
-import { lookupListToken, lookupLists } from './providers';
-import { MockXHRBackend } from './mock-xhr-backend';
-import { routing } from './app.routing';
-import { NewItemModule } from './new-item/new-item.module';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { HttpClientModule, HttpXhrBackend } from "@angular/common/http";
+import { AppComponent } from "./app.component";
+import { MediaItemComponent } from "./media-item.component";
+import { MediaItemListComponent } from "./media-item-list.component";
+import { FavoriteDirective } from "./favorite.directive";
+import { CategoryListPipe } from "./category-list.pipe";
+import { lookupListToken, lookupLists } from "./providers";
+import { MockXHRBackend } from "./mock-xhr-backend";
+import { routing } from "./app.routing";
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    routing,
-    NewItemModule
-  ],
+  imports: [BrowserModule, HttpClientModule, routing],
   declarations: [
     AppComponent,
     MediaItemComponent,
     MediaItemListComponent,
     FavoriteDirective,
-    CategoryListPipe
+    CategoryListPipe,
   ],
   providers: [
     { provide: lookupListToken, useValue: lookupLists },
-    { provide: HttpXhrBackend, useClass: MockXHRBackend }
+    { provide: HttpXhrBackend, useClass: MockXHRBackend },
   ],
-  bootstrap: [
-    AppComponent
-  ]
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,9 +1,14 @@
-import { Routes, RouterModule } from '@angular/router';
-import { MediaItemListComponent } from './media-item-list.component';
+import { Routes, RouterModule } from "@angular/router";
+import { MediaItemListComponent } from "./media-item-list.component";
 
 const appRoutes: Routes = [
-  { path: ':medium', component: MediaItemListComponent },
-  { path: '', pathMatch: 'full', redirectTo: 'all' }
+  {
+    path: "add",
+    loadChildren: () =>
+      import("./new-item/new-item.module").then((m) => m.NewItemModule),
+  },
+  { path: ":medium", component: MediaItemListComponent },
+  { path: "", pathMatch: "full", redirectTo: "all" },
 ];
 
 export const routing = RouterModule.forRoot(appRoutes);
