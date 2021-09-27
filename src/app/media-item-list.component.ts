8d8dce6c8a16865e266ccde744e8fdff705332e2
Vignesh KRV <vignesh1105krv@gmail.com>
2021-09-27 15:41:56 -0500

    route parameters

--- src/app/media-item-list.component.ts
+++ src/app/media-item-list.component.ts
@@ -1,33 +1,41 @@
-import { Component, OnInit } from '@angular/core';
-import { MediaItemService, MediaItem } from './media-item.service';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { MediaItemService, MediaItem } from "./media-item.service";
 
 @Component({
-  selector: 'mw-media-item-list',
-  templateUrl: './media-item-list.component.html',
-  styleUrls: ['./media-item-list.component.css']
+  selector: "mw-media-item-list",
+  templateUrl: "./media-item-list.component.html",
+  styleUrls: ["./media-item-list.component.css"],
 })
 export class MediaItemListComponent implements OnInit {
-  medium = '';
+  medium = "";
   mediaItems: MediaItem[];
 
-  constructor(private mediaItemService: MediaItemService) {}
+  constructor(
+    private mediaItemService: MediaItemService,
+    private activateRoute: ActivatedRoute
+  ) {}
 
   ngOnInit() {
-    this.getMediaItems(this.medium);
+    this.activateRoute.paramMap.subscribe((paramMap) => {
+      let medium = paramMap.get("medium");
+      if (medium.toLowerCase() === "all") {
+        medium = "";
+      }
+      this.getMediaItems(medium);
+    });
   }
 
   onMediaItemDelete(mediaItem: MediaItem) {
-    this.mediaItemService.delete(mediaItem)
-      .subscribe(() => {
-        this.getMediaItems(this.medium);
-      });
+    this.mediaItemService.delete(mediaItem).subscribe(() => {
+      this.getMediaItems(this.medium);
+    });
   }
 
   getMediaItems(medium: string) {
     this.medium = medium;
-    this.mediaItemService.get(medium)
-      .subscribe(mediaItems => {
-        this.mediaItems = mediaItems;
-      });
+    this.mediaItemService.get(medium).subscribe((mediaItems) => {
+      this.mediaItems = mediaItems;
+    });
   }
 }
